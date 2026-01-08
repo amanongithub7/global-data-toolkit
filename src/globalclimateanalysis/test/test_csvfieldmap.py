d67fe1147095fc9d9a2fe383960e6d502827cccf
Aman Oberoi <github.com.petroleum369@passfwd.com>
2026-01-08 20:09:21 +0530

    test(dataset/csvfieldmap): unit test for key error case in csv field-to-snake-case conversion

--- src/globalclimateanalysis/test/test_csvfieldmap.py
+++ src/globalclimateanalysis/test/test_csvfieldmap.py
@@ -5,12 +5,16 @@ from globalclimateanalysis.dataset import csvfieldmap
 
 class TestConvertCsvFieldNameToSnakeCase(unittest.TestCase):
     def test_existing_field_conversion(self):
-        snake_version = csvfieldmap.convert_csv_field_name_to_snake_case(
+        func_snake_version = csvfieldmap.convert_csv_field_name_to_snake_case(
             "Forest Area (%)"
         )
         expected_snake_version = "forest_area_%"
-        self.assertEqual(snake_version, expected_snake_version)
+        self.assertEqual(func_snake_version, expected_snake_version)
 
-
-# if __name__ == "__main__":
-#     unittest.main()
+    def test_exception_on_unknown_field_conversion(self):
+        self.assertRaises(
+            KeyError,
+            csvfieldmap.convert_csv_field_name_to_snake_case,
+            # a field that doesn't exist in the dataset
+            "Cropland (%)",
+        )
