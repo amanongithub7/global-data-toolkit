0de5bdb02a74dd64bb04df0719aa3345950d4334
Aman Oberoi <github.com.petroleum369@passfwd.com>
2026-04-16 08:37:17 +0530

    docs: add package and module level docstrings

---
+++ src/globaldatatoolkit/__init__.py
@@ -0,0 +1,16 @@
+"""
+Global Data Toolkit
+===================
+
+Utilities for managing global datasets.
+
+How to use the documentation
+----------------------------
+Documentation is available in two forms: docstrings provided
+with the code, and an API reference guide.
+
+Available subpackages
+---------------------
+dataset
+    Data loader for country-specific pandas.DataFrame objects from a main csv
+"""
---
+++ src/globaldatatoolkit/dataloaders/__init__.py
@@ -0,0 +1,11 @@
+"""
+Data Loaders
+============
+
+Data loaders for global datasets.
+
+Available modules
+-----------------
+bycountry
+    Data loader for country name to pandas.DataFrame map loading.
+"""
